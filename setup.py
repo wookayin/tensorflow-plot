e7791c6f72cfbc07083ed6075ee61260aaa51af7
Jongwook Choi <wookayin@gmail.com>
2017-04-06 00:50:19 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+__version__ = '0.1.0.dev0'
+
+import codecs
+from setuptools import setup
+
+readme = codecs.open('README.md', encoding='utf-8').read()
+
+
+setup(
+    name='tfplot',
+    version=__version__,
+    description='TensorFlow Plot',
+    long_description=readme,
+    url='https://github.com/wookayin/tensorflow-plot',
+    author='Jongwook Choi',
+    author_email='wookayin@gmail.com',
+    keywords='tensorflow matplotlib tensorboard plot tfplot',
+    packages=[
+        'tfplot',
+    ],
+    install_requires=[
+        'numpy',
+        'matplotlib',
+    ],
+    classifiers=[
+        # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+    ],
+    include_package_data=True,
+    license='MIT License',
+)
